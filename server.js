fbe4369546a045414f0faea59f5d6baee7c33db8
Annie Nguyen <anguyen.aln@gmail.com>
2021-12-22 23:43:01 -0800

    added connection

--- server.js
+++ server.js
@@ -4,5 +4,10 @@ const cTable = require("console.table");
 require('dotenv')
 
 //connection
-
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: process.env.MYSQL_PASSWORD,
+    database: 'employeeTracker_db'
+  });
 //inquirer 
