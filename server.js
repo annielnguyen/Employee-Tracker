d55cf58a95c955ed58e75a2df25729aaa9dd9b27
Annie Nguyen <anguyen.aln@gmail.com>
2021-12-23 13:39:46 -0800

    added function for viewing list of dept and roles

--- server.js
+++ server.js
@@ -7,7 +7,30 @@ require('dotenv')
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: process.env.MYSQL_PASSWORD,
+    password: 'UCLA123!',
     database: 'employeeTracker_db'
   });
+
+  connection.connect((err) => {
+    if (err) throw err;
+    init();
+});
 //inquirer 
+const viewDepartments = () => {
+    const query = 'SELECT * FROM department';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        init();
+    })
+}
+
+const viewRoles = () => {
+    const query = 'SELECT * FROM role';
+    connection.query(query, (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        init();
+    })
+}
+
