21c5db2e4b5045a3043f7aabc738e584f39e976f
Annie Nguyen <anguyen.aln@gmail.com>
2021-12-23 14:26:35 -0800

    added update employee function and the start menu prompt

--- server.js
+++ server.js
@@ -83,3 +83,79 @@ const addRole = () => {
         })
     })
 }
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "What is the employee's first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "What is the employee's last name?"
+        },
+        {
+            type: "input",
+            name: "roleId",
+            message: "Enter a number for their role ID"
+        },
+        {
+            type: "input",
+            name: "managerId",
+            message: "Enter their mananger's ID or 'null' if there is no manager"
+        }
+    ]).then((answer) => {
+        const query = 'INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUE (?, ?, ?, ?)';
+        connection.query(query, [answer.firstName, answer.lastName, answer.roleId, answer.managerId], (err, res) => {
+            if (err) throw err;
+            listEmployees();
+            init();
+        })
+    })
+}
+
+const updateEmployee= () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "employeeId",
+            message: "Enter the employee ID would you like to update"
+        },
+        {
+            type: "input",
+            name: "roleId",
+            message: "What is the ID of their new role?"
+}
+const init = () => {
+    inquirer.prompt({
+        type: "list",
+        name: "select",
+        message: "Select from the choices below",
+        choices: [
+            "View company departments",
+            "View company roles",
+            "View employee roster",
+            "Add a department",
+            "Add a role",
+            "Add employee to roster",
+            "UpdateEmployee"
+        ]
+    }).then((answer) => {
+        if (answer.select === "View company departments") {
+            listDepartments();
+        } else if (answer.select === "View company roles") {
+            listRoles();
+        } else if (answer.select === "View employee roster") {
+            listEmployees();
+        } else if (answer.select === "Add a department") {
+            addDepartment();
+        } else if (answer.select === "Add a role") {
+            addRole();
+        } else if (answer.select === "Add employee to roster") {
+            addEmployee();
+        } else if (answer.select === "Update an employee") {
+        }
+    })
+}
